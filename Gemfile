e1321c44e9f6185bbf18d724105969c84037bf7d
ilovearsenal93 <ongsatthu@gmail.com>
2014-06-19 21:05:39 +0700

    Modeling users

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.5'
-
+gem 'bcrypt-ruby', '3.1.2'
 # Use sqlite3 as the database for Active Record
 
 # Use SCSS for stylesheets
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
-describe "UserPages" do
-    describe "signup page" do
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
     before { visit signup_path }
+
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
-  end
-    end
   end
 end
